3c43c2ea2250c050f8c0bca675b878566a9f47ee
Adrian Navarrete <a.navarrete.uk@gmail.com>
2018-06-20 23:03:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -37,5 +37,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  return (array.slice(0, array.lenght - 1))
+  array.slice(0, array.lenght - 1)
+  return array
 }
